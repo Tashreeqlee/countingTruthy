9854e31b9aecd072042bc69b3daaa99f4f662c3e
Tashreeqlee <tashyct14@gmail.com>
2023-11-27 14:47:05 +0200

    css and javascript added

--- script/code.js
+++ script/code.js
@@ -1,1 +1,4 @@
-//
+let booleanArray = [true, true, true, false, true, true, false];
+let trueCount = booleanArray.filter(Boolean).length;
+
+document.getElementById("trueCount").textContent = trueCount;
